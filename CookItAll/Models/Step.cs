2020bd476570885a09870b9264f15828c4d64b85
Daniel Madsen <steamblacksej2@gmail.com>
2022-09-05 15:10:52 +0200

    Fix

--- CookItAll/Models/Step.cs
+++ CookItAll/Models/Step.cs
@@ -1,10 +1,13 @@
-﻿namespace CookItAll.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CookItAll.Models
 {
     public class Step
     {
         public int Id { get; set; }
         public string Description { get; set; }
         public Recipe Recipe { get; set; }
+        [ForeignKey("Step")]
         public Step NextStep { get; set; }
     }
 }

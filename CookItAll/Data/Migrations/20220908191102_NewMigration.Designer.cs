6c9ed86e36247d3a149285ea591e2e03ccab82a7
Daniel Madsen <steamblacksej2@gmail.com>
2022-09-08 21:20:27 +0200

    Moved from local DB to school DB

--- CookItAll/Data/Migrations/20220908191102_NewMigration.Designer.cs
+++ CookItAll/Data/Migrations/20220908191102_NewMigration.Designer.cs
@@ -4,6 +4,7 @@ using CookItAll.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CookItAll.Data.Migrations
 {
     [DbContext(typeof(CookItAllContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220908191102_NewMigration")]
+    partial class NewMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CookItAll/Data/Migrations/20220908191102_NewMigration.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CookItAll.Data.Migrations
+{
+    public partial class NewMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Ingredient_Category_CategoryId",
+                table: "Ingredient");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_IngredientAmount_Ingredient_IngredientId",
+                table: "IngredientAmount");
+
+            migrationBuilder.RenameColumn(
+                name: "IngredientId",
+                table: "IngredientAmount",
+                newName: "IngredientID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_IngredientAmount_IngredientId",
+                table: "IngredientAmount",
+                newName: "IX_IngredientAmount_IngredientID");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Ingredient",
+                newName: "ID");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoryId",
+                table: "Ingredient",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Ingredient_Category_CategoryId",
+                table: "Ingredient",
+                column: "CategoryId",
+                principalTable: "Category",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_IngredientAmount_Ingredient_IngredientID",
+                table: "IngredientAmount",
+                column: "IngredientID",
+                principalTable: "Ingredient",
+                principalColumn: "ID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Ingredient_Category_CategoryId",
+                table: "Ingredient");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_IngredientAmount_Ingredient_IngredientID",
+                table: "IngredientAmount");
+
+            migrationBuilder.RenameColumn(
+                name: "IngredientID",
+                table: "IngredientAmount",
+                newName: "IngredientId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_IngredientAmount_IngredientID",
+                table: "IngredientAmount",
+                newName: "IX_IngredientAmount_IngredientId");
+
+            migrationBuilder.RenameColumn(
+                name: "ID",
+                table: "Ingredient",
+                newName: "Id");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoryId",
+                table: "Ingredient",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Ingredient_Category_CategoryId",
+                table: "Ingredient",
+                column: "CategoryId",
+                principalTable: "Category",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_IngredientAmount_Ingredient_IngredientId",
+                table: "IngredientAmount",
+                column: "IngredientId",
+                principalTable: "Ingredient",
+                principalColumn: "Id");
+        }
+    }
+}
